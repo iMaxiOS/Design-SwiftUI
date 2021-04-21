7e416518d0f6d0a6ad01688279bd215efcdebc57
Macostik <y.granchenko@gmail.com>
2021-04-21 12:20:30 +0300

    create Project

---
+++ Design-SwiftUI/Design_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Design_SwiftUIApp.swift
+//  Design-SwiftUI
+//
+//  Created by Maxim Granchenko on 21.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct Design_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
